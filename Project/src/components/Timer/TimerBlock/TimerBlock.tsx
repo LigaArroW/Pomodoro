49512141eb2557c57fc4e6cb7dc78e68d7e5e206
Romanov Vitaly <9773411353@mail.ru>
2024-05-14 14:58:36 +0300

    doing

--- Project/src/components/Timer/TimerBlock/TimerBlock.tsx
+++ Project/src/components/Timer/TimerBlock/TimerBlock.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import styles from './TimerBlock.module.css';
 import { useTimerControl } from '../../../hooks/useTimerControl';
 import { task, useTask } from '../../../store/useTask';
@@ -9,26 +9,21 @@ interface TimerBlockProps {
 }
 
 export const TimerBlock: FC<TimerBlockProps> = ({ task }) => {
-  const [isPaused, setIsPaused] = useState<boolean>(false)
-  const { timer, paused, stop } = useTimerControl(task)
-  const minute = Math.floor(timer / (1000 * 60) % 60)
-  const second = Math.floor(timer / (1000) % 60)
-  const increment = useTask(state => state.inrementTime)
 
-  const handlePause = () => {
-    // if (task.id === 0) return
-    paused()
-    setIsPaused(!isPaused)
-  }
-  const handleStop = () => {
-    // if (task.id === 0) return
-    stop()
-  }
+  // const minute = Math.floor(timer / (1000 * 60) % 60)
+  // const second = Math.floor(timer / (1000) % 60)
+  // const increment = useTask(state => state.inrementTime)
 
+  useEffect(() => {
+
+  })
+
+  
 
   return (
     <div className={styles.timerBlock}>
-      <div className={`${styles.headerTime} ${!isPaused ? styles.red : styles.green}`}>
+      <div className={`${styles.headerTime} `}>
+      {/* <div className={`${styles.headerTime} ${!isPaused ? styles.red : styles.green}`}> */}
         <span className={styles.headerTitle}>
           {/* Сверстать сайт */}
           {task.task}
@@ -39,10 +34,13 @@ export const TimerBlock: FC<TimerBlockProps> = ({ task }) => {
       </div>
       <div className={styles.content}>
         <div className={styles.timer}>
-          <span className={`${styles.timerClock} ${!isPaused ? styles.red : styles.green}`}>
-            {`${minute}:${second.toString().length === 1 ? `0${second}` : second}`}
+          <span className={`${styles.timerClock} `}>
+          {/* <span className={`${styles.timerClock} ${!isPaused ? styles.red : styles.green}`}> */}
+            {/* {`${minute}:${second.toString().length === 1 ? `0${second}` : second}`} */}
+            00:00
           </span>
-          <span onClick={() => increment(task)} className={styles.timmerAdd}>
+          <span className={styles.timmerAdd}>
+          {/* <span onClick={() => increment(task)} className={styles.timmerAdd}> */}
             <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
               <circle cx="25" cy="25" r="25" fill="#C4C4C4" />
               <path d="M26.2756 26.1321V33H23.7244V26.1321H17V23.7029H23.7244V17H26.2756V23.7029H33V26.1321H26.2756Z" fill="white" />
@@ -54,8 +52,8 @@ export const TimerBlock: FC<TimerBlockProps> = ({ task }) => {
           <span className={styles.taskName}>{task.task}</span>
         </div>
         <div className={styles.btnContainer}>
-          <button onClick={handlePause} className={styles.btnSucsess}>{isPaused ? 'Пауза' : 'Старт'}</button>
-          <button onClick={handleStop} className={styles.btnEnd}>Стоп</button>
+          {/* <button onClick={handlePause} className={styles.btnSucsess}>{isPaused ? 'Пауза' : 'Старт'}</button>
+          <button onClick={handleStop} className={styles.btnEnd}>Стоп</button> */}
         </div>
       </div>
     </div>
--- Project/src/hooks/useTimerControl.ts
+++ Project/src/hooks/useTimerControl.ts
@@ -7,41 +7,45 @@ import { task, useTask } from "../store/useTask"
 
 export const useTimerControl = (task: task) => {
     const [isRunning, setisRunning] = useState<boolean>(false)
-    const [timer, setTimer] = useState(0)
-    const editTimer = useTask(state => state.editTime)
-    const timeRefSeconds = useRef(timer)
-    const intervalRef = useRef(0)
-
-    useEffect(() => {
-        setTimer(task.timer)
-    }, [task.timer])
-
-    useEffect(() => {
-        if (isRunning) {
-            intervalRef.current = setInterval(() => {
-                timeRefSeconds.current -= 1
-                // console.log(timeRefSeconds.current);
-                setTimer(timeRefSeconds.current * 1000)
-            }, 1000)
-        }
-
-        return () => {
-            clearInterval(intervalRef.current)
-
-        }
-    }, [isRunning])
-
-    const paused = () => {
-        setisRunning(!isRunning)
-        timeRefSeconds.current = timer / 1000
-        editTimer(timer, task.task)
-    }
-
-    const stop = () => {
-        setisRunning(false)
-        setTimer(DEFAULT_TIME)
-        timeRefSeconds.current = timer / 1000
-    }
-
-    return { timer, paused, stop }
+    
 }
+// export const useTimerControl = (task: task) => {
+//     const [isRunning, setisRunning] = useState<boolean>(false)
+//     const [timer, setTimer] = useState(0)
+//     const editTimer = useTask(state => state.editTime)
+//     const timeRefSeconds = useRef(timer)
+//     const intervalRef = useRef(0)
+
+//     useEffect(() => {
+//         setTimer(task.timer)
+//     }, [task.timer])
+
+//     useEffect(() => {
+//         if (isRunning) {
+//             intervalRef.current = setInterval(() => {
+//                 timeRefSeconds.current -= 1
+//                 // console.log(timeRefSeconds.current);
+//                 setTimer(timeRefSeconds.current * 1000)
+//             }, 1000)
+//         }
+
+//         return () => {
+//             clearInterval(intervalRef.current)
+
+//         }
+//     }, [isRunning])
+
+//     const paused = () => {
+//         setisRunning(!isRunning)
+//         timeRefSeconds.current = timer / 1000
+//         editTimer(timer, task.task)
+//     }
+
+//     const stop = () => {
+//         setisRunning(false)
+//         setTimer(DEFAULT_TIME)
+//         timeRefSeconds.current = timer / 1000
+//     }
+
+//     return { timer, paused, stop }
+// }
