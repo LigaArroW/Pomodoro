8d09b2cc83a52b13952ea27d6740d33a34e9f9b0
Romanov Vitaly <9773411353@mail.ru>
2024-05-17 01:30:01 +0300

    doing

--- Project/src/components/Statistic/Statistic.tsx
+++ Project/src/components/Statistic/Statistic.tsx
@@ -1,14 +1,27 @@
 import { FC } from 'react';
 import styles from './Statistic.module.css';
+import ReactSelect, { OnChangeValue, Options } from 'react-select';
+import { OPTIONS_SELECT } from '../../constants/OPTIONS_SELECT';
 
 interface StatisticProps { }
 
 export const Statistic: FC<StatisticProps> = () => {
+
+  const handleChange = (e: OnChangeValue<{ value: string; label: string; }, false>) => {
+    console.log(e)
+  }
+
   return (
     <div className={styles.statistic}>
-     <div className={styles.header}>
-      <h2 className={styles.title}>Ваша активность</h2>
-     </div>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Ваша активность</h2>
+        <ReactSelect
+          className={styles.select}
+          options={OPTIONS_SELECT}
+          defaultValue={OPTIONS_SELECT[0]}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   )
 };
---
+++ Project/src/constants/OPTIONS_SELECT.ts
@@ -0,0 +1,5 @@
+export const OPTIONS_SELECT = [
+    { value: 'this-week', label: 'Эта неделя' },
+    { value: 'last-week', label: 'Прошлая неделя' },
+    { value: '2-week', label: '2 недели назад' }
+]
