a7fc036d2324cdc8f24cbeb4eafa0015623355b0
vitalii_romanov <9773411353@mail.ru>
2024-05-23 02:10:32 +0300

    remove

--- Project/src/constants/DEFAULT_TIME.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_TIME = 1000 * 60 * 25
-export const MINUTE = 1000 * 60
--- Project/src/utils/timeFormat.ts
+++
@@ -1,6 +0,0 @@
-export const timeFormat = (time: number) => {
-    const hour = Math.floor(time / (1000 * 60 * 60) % 60)
-    const munites = Math.floor(time / (1000 * 60) % 60)
-    // return `${hour} час ${munites} мин`
-    return `${hour > 0 ? hour + ' час' : ''} ${munites > 0 ? munites + 'мин' : ''}`
-}
